0581d63a1e02a61d7336ace43507ee7eb4520ca1
colton <lwcolton@gmail.com>
2017-08-08 22:18:05 -0500

    Add code and some tests

---
+++ tests/test_channel.py
@@ -0,0 +1,54 @@
+from unittest import TestCase, mock
+from uuid import uuid4
+
+from message_api.channel import MessageApi
+
+class TestChannelCreate(TestCase):
+    def setUp(self):
+        self.project_id = str(uuid4().hex)
+        self.api_key = str(uuid4().hex)
+        self.message_api = MessageApi(
+            project_id=self.project_id,
+            api_key=self.api_key
+        )
+
+    def test_channel_create_success(self):
+        channel_token = str(uuid4().hex)
+        self.message_api._send_request = mock.Mock()
+        self.message_api._send_request.return_value = {
+            "token":  channel_token
+        }
+        client_id = str(uuid4().hex)
+        result = self.message_api.create_channel(client_id)
+        self.assertEqual(channel_token, result)
+        params = {
+            "project_id": self.project_id
+        }
+        body = {
+            "channel_id": client_id
+        }
+
+        self.message_api._send_request.assert_called_once_with(
+            "post",
+            params=params,
+            json=body
+        )
+
+    def test_send_message_success(self):
+        self.message_api._send_request = mock.Mock()
+        client_id = str(uuid4().hex)
+        message = str(uuid4().hex)
+        self.message_api.send_message(client_id, message)
+        params = {
+            "project_id": self.project_id
+        }
+        body = {
+            "channel_id": client_id,
+            "message": message
+        }
+
+        self.message_api._send_request.assert_called_once_with(
+            "post",
+            params=params,
+            json=body
+        )
